3ebffe226e1244f1acbeb287a63672cbeb9a99e6
Vishnu Jain <121681876+Vishnuujain@users.noreply.github.com>
2024-07-15 11:05:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
     return render_template('index.html')
 
+# This is for local development
 if __name__ == '__main__':
     app.run(debug=True)
+
+# This is for Vercel
+app = app
